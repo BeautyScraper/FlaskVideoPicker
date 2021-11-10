dc919a1ac4ca82a6d3b023cf54c5eec9000f3677
Your Name <you@example.com>
2021-11-10 15:21:53 +0530

    index page working

--- fpicker.py
+++ fpicker.py
@@ -10,14 +10,24 @@ import pandas as pd
 
 from bluevs import vsblueprint
 
+app = Flask(__name__)
+
+cnfLinks = []
+
+@app.route('/')
+def main():
+    print(cnfLinks)
+    return render_template('index.html',links=cnfLinks)
+    
+
 if __name__ == '__main__':
-    app = Flask(__name__)
     cnffiledir = Path.cwd() / 'configs'
     for cnffile in cnffiledir.glob('*.cfg'):
         print('hi')
         x = vsblueprint()
         bp = x.createbp(str(cnffile))
         app.register_blueprint(bp,url_prefix='/%s' % cnffile.stem)
+        cnfLinks.append(cnffile.stem)
     app.run(host='0.0.0.0',port=5002)
     print(app.url_map)
 # inputDir = r'D:\paradise\stuff\Images\walls'
